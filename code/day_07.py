655867281fbac526e0a2e469ec8b87776a1385d7
Christopher Cheng <39285519+CCJK123@users.noreply.github.com>
2021-12-07 05:30:24 +0000

    Added day 7

--- code/day_07.py
+++ code/day_07.py
@@ -1,2 +1,23 @@
 def day_07(input_str):
-    pass
+    # Initial Setup
+    input_array = input_str.split(",")
+    input_array = [int(i) for i in input_array]
+    outputs = []
+
+    # Part 1
+    costs = [0]*(max(input_array)+1)
+    for i in range(len(costs)):
+        for j in input_array:
+            costs[i] += abs(i-j)
+    outputs.append(min(costs))
+
+    # Part 2
+    def one_to_x_sum(x):
+        return int((x+1)*(x/2))
+    costs = [0]*(max(input_array)+1)
+    for i in range(len(costs)):
+        for j in input_array:
+            costs[i] += one_to_x_sum(abs(i-j))
+    outputs.append(min(costs))
+
+    return outputs
