6ce36c90b119b727f9286fb9755da648736264de
Christopher Cheng <39285519+CCJK123@users.noreply.github.com>
2021-12-10 09:23:43 +0000

    Reimplemented part of exception handling in main.py to be better for debugging

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ try:
     day_no = int(day_no)
 
     if day_no in range(1, 26):
+        
         day_str = f"day_{str(day_no).zfill(2)}"
         exec(f"from code.{day_str} import {day_str}")
         with open(f"inputs/{day_str}_input.txt", "r") as fp:
             input_str = fp.read()
             outputs = eval(f"{day_str}(input_str)")
-        for i in range(len(outputs)):
-            print(f"Part {i+1}: {outputs[i]}")
+        
+        if outputs == None:
+            print("The code does not return anything!")
+        else:
+            for i in range(len(outputs)):
+                print(f"Part {i+1}: {outputs[i]}")
     
     else:
         print("That is not a valid number!")
 
 except ValueError:
     print("That is not a number!")
-
-except TypeError:
-    print("The code does not return anything!")
